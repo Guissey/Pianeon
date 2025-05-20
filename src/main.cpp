2461a4265ca4994d29c8c8948b1ec64fa0d6742a
Guissey <cedricollivier96@gmail.com>
2025-05-21 01:58:47 +0200

    change board config

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 
 #include "LedController.h"
 #include "ConfigServer.h"
@@ -7,7 +6,7 @@
 #define PIN_WS2812B 16
 #define LED_NUMBER 175
 #define USE_PREFERENCES 0
-#define WEBSERVER_MODE 2 // 0: inactive, 1: AP, 2: STA
+#define WEBSERVER_MODE 1 // 0: inactive, 1: AP, 2: STA
 
 /// Initialisation ///
 
@@ -24,7 +23,7 @@ void setup() {
 
 /// Loop ///
 void loop() {
-  // led.blinkLoop();
+  led.blinkLoop();
   led.show();
   server.loop();
 }
